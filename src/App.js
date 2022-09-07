8ea5587c76078ad7e819b0e6ad7c82efdc9045f5
Nneka-Chukwuemeka <61991990+Nneka-Chukwuemeka@users.noreply.github.com>
2022-09-07 23:09:20 +0100

    use effect context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 import * as React from 'react';
+const themes = {
+  light: {
+    foreground: '#000000',
+    background: '#eeeeee',
+  },
+  dark: {
+    foreground: '#ffffff',
+    background: '#222222',
+  },
+};
 
-const ThemeContext = React.createContext();
+const ThemeContext = React.createContext(themes.light);
 
-export default function App() {}
+export default function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+  const changeValue = darkMode ? themes.dark : themes.light;
+
+  console.log(changeValue);
+  return (
+    <ThemeContext.Provider value={changeValue}>
+      <Content changeTheme={setDarkMode} Theme={darkMode} />
+    </ThemeContext.Provider>
+  );
+}
+function Content({ changeTheme, Theme }) {
+  const toggleTheme = () => {
+    changeTheme(!Theme);
+  };
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div
+      style={{
+        padding: '10rem 9rem',
+        width: '100%',
+        height: '100vh',
+        background: theme.background,
+        color: theme.foreground,
+      }}
+    >
+      <button onClick={toggleTheme}>change Setting </button>
+    </div>
+  );
+}
